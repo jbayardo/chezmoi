f5b93dbf061c236f46f3e126571e2d58d0942a13
Julian Bayardo Spadafora <julian@bayardo.info>
2026-02-22 20:19:39 -0800

    update

--- executable_update.sh
+++ executable_update.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y
 ssh garfio 'sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y'
+ssh blacktail 'sudo apt-get update && sudo apt-get dist-upgrade -y && sudo apt-get autoremove -y'
